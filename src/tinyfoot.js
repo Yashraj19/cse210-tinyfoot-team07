448e44f6fb72cabe6dd0b8e2a01515ceb5990bdc
Zhenge Chen <zhc064@ucsd.edu>
2024-10-16 16:52:30 -0700

    feat: Implement numeric style for buttons
    Numeric style buttons have the class "numeric". This library also needs
    to be initialized now to pass initialization options. See examples in
    demo.html.

--- src/tinyfoot.js
+++ src/tinyfoot.js
@@ -1,55 +1,80 @@
-let activePopup = null;
-const template = document.getElementById("tinyfoot-template");
+class TinyfootFootnote extends HTMLElement {
+  // current active popup
+  static #activePopup = null;
 
-customElements.define(
-  "tinyfoot-footnote",
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const root = this.attachShadow({ mode: "open" });
-      root.appendChild(template.content.cloneNode(true));
-      root
-        .getElementById("tinyfoot-button")
-        .addEventListener("click", this.onClick.bind(this));
-      root
-        .getElementById("tinyfoot-popup")
-        .addEventListener("click", (event) => event.stopPropagation());
-    }
+  // template used to create footnote elements
+  static #template = document.getElementById("tinyfoot-template");
 
-    onClick(event) {
-      const thisPopup = this.shadowRoot.getElementById("tinyfoot-popup");
-      if (activePopup === thisPopup) {
-        activePopup = null;
-        thisPopup.classList.remove("active");
-      } else {
-        if (activePopup !== null) {
-          activePopup.classList.remove("active");
-        }
-        thisPopup.classList.add("active");
-        activePopup = thisPopup;
+  // whether the button will be numbered
+  static numeric = false;
 
-        // Prevent the popup from overflowing
-        for (const c of ["left-edge", "right-edge", "bottom-edge"]) {
-          activePopup.classList.remove(c);
-        }
-        const rect = thisPopup.getBoundingClientRect();
-        if (rect.left < 0) {
-          thisPopup.classList.add("left-edge");
-        } else if (rect.left + rect.width > window.innerWidth) {
-          thisPopup.classList.add("right-edge");
-        } else if (rect.y + rect.height > window.innerHeight) {
-          thisPopup.classList.add("bottom-edge");
-        }
+  // whether the popup will show in the bottom
+  static bottom = false;
+
+  static {
+    window.addEventListener("click", () => {
+      // click on other parts to close the popup
+      if (TinyfootFootnote.#activePopup != null) {
+        TinyfootFootnote.#activePopup.classList.remove("active");
+        TinyfootFootnote.#activePopup = null;
       }
-
-      event.stopPropagation();
-    }
-  },
-);
-
-window.addEventListener("click", () => {
-  if (activePopup != null) {
-    activePopup.classList.remove("active");
-    activePopup = null;
+    });
   }
-});
+
+  constructor() {
+    super();
+    const root = this.attachShadow({ mode: "open" });
+    root.appendChild(TinyfootFootnote.#template.content.cloneNode(true));
+
+    const button = root.getElementById("tinyfoot-button");
+    const popup = root.getElementById("tinyfoot-popup");
+    button.addEventListener("click", this.onClick.bind(this));
+    if (TinyfootFootnote.numeric) {
+      button.classList.add("numeric");
+    }
+    popup.addEventListener("click", (event) => event.stopPropagation());
+    if (TinyfootFootnote.bottom) {
+      popup.classList.add("buttom");
+    }
+  }
+
+  onClick(event) {
+    event.stopPropagation();
+    const thisPopup = this.shadowRoot.getElementById("tinyfoot-popup");
+
+    if (TinyfootFootnote.#activePopup === thisPopup) {
+      // click on the same button to close the popup
+      TinyfootFootnote.#activePopup = null;
+      thisPopup.classList.remove("active");
+      return;
+    }
+
+    if (TinyfootFootnote.#activePopup !== null) {
+      TinyfootFootnote.#activePopup.classList.remove("active");
+    }
+    thisPopup.classList.add("active");
+    TinyfootFootnote.#activePopup = thisPopup;
+
+    // prevent the popup from overflowing
+    if (thisPopup.classList.contains("bottom")) {
+      return;
+    }
+    for (const c of ["left-edge", "right-edge", "bottom-edge"]) {
+      thisPopup.classList.remove(c);
+    }
+    const rect = thisPopup.getBoundingClientRect();
+    if (rect.left < 0) {
+      thisPopup.classList.add("left-edge");
+    } else if (rect.left + rect.width > window.innerWidth) {
+      thisPopup.classList.add("right-edge");
+    } else if (rect.y + rect.height > window.innerHeight) {
+      thisPopup.classList.add("bottom-edge");
+    }
+  }
+}
+
+export default function (options) {
+  TinyfootFootnote.numeric = Boolean(options?.numeric);
+  TinyfootFootnote.bottom = Boolean(options?.bottom);
+  customElements.define("tinyfoot-footnote", TinyfootFootnote);
+}
